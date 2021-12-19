1215e4ac3eab388aae5ac8c684abe8e6e6f20073
prangancode <senprangan@gmail.com>
2021-12-19 20:03:36 +0600

    first commit

---
+++ routes/stripe.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const stripe = require("stripe")("sk_test_51K7lyoLxhKYmdYyraqYuBIyKv0d9kxobqmuVG99hlwzkf0e9kwl6SLl1avHOXeKs37RYPDsMEFigsFq4JgncazRi00qOHttwLi");
+
+router.post("/payment", (req, res) => {
+    stripe.charges.create(
+        {
+            source: req.body.tokenId,
+            amount: req.body.amount,
+            currency: "usd",
+        },
+        (stripeErr, stripeRes) => {
+            if (stripeErr) {
+                res.status(500).json(stripeErr);
+            } else {
+                res.status(200).json(stripeRes);
+            }
+        }
+    );
+});
+
+module.exports = router;
